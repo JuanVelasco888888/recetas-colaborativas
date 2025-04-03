6762b50dbdf4d4ddf95e11c3959e3704f978f666
DanielCamiloRamosGarciaCAMPUS <danipro456@hotmail.com>
2025-04-03 07:18:00 -0500

    Primer commit - Creacion Estructura

---
+++ src/recetas/colaborativas/RecetasColaborativas.java
@@ -0,0 +1,15 @@
+
+package recetas.colaborativas;
+
+/**
+ *
+ * @author daniel
+ */
+public class RecetasColaborativas {
+
+    public static void main(String[] args) {
+            System.out.println("¡Bienvenido al sistema de recetas!");
+
+    }
+    
+}
